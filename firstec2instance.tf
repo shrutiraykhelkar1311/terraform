056ff71296dc1500cc68aea93021ce9dbaa9879e
shrutiraykhelkar1311~ <shrutiraykhelkar@gmail.com~>
2024-03-06 17:57:02 +0530

    creating a first ec2 instance

---
+++ firstec2instance.tf
@@ -0,0 +1,19 @@
+terraform{
+      required_providers {
+      aws = { 
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+}
+required_version = ">= 1.2.0"
+}
+provider"aws"{
+        region = "us-east-1"
+}
+resource "aws_instance" "my_ec2_instance" {
+         ami="ami-07d9b9ddc6cd8dd30"
+         instance_type="t2.micro"
+         tags={
+         Name="terraformbatch"
+         }
+}
